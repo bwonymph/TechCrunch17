fcf7fac1d2edf881159f90acfd07dcb6392352ca
Kent Makishima <khmakishima@ucdavis.edu>
2017-09-17 02:16:32 -0700

    Added locations taken from Satori server

--- Backend/server.py
+++ Backend/server.py
@@ -1,10 +1,19 @@
 #server.py
 
 import nexmo
+import time
+from __future__ import print_function
+import time
+from satori.rtm.client import make_client
+
+global locationem
+
+endpoint = ""
+appkey = ""
 
 client = nexmo.Client(key='', secret='')
 
-# client = nexmo.Client(application_id='a7759aa3-88ef-4bcf-abc3-3c0334cec861', private_key= './private.key')
+#client = nexmo.Client(application_id='--', private_key= './private.key')
 
 response = client.send_message({'from': '', 'to': '', 'text': 'Are you in a Safe location? What is your location?'})
 
@@ -17,8 +26,53 @@ if response['status'] == '0':
 else:
   print 'Error:', response['error-text']
 
-# response = client.create_call({
-#   'to': [{'type': 'phone', 'number': '19169908919'}],
-#   'from': {'type': 'phone', 'number': '12012413501'},
-#   'answer_url': ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json']
-# })
+time.sleep(10)
+
+class SubscriptionObserver(object): #Get location to go to from mapping 
+    def on_enter_subscribed(self):
+        print('Subscribed to: locations')
+
+    def on_subscription_data(self, pdu):
+        for service1 in pdu['messages']:
+            locationem = messages[location]
+
+
+    def on_enter_failed(self, reason):
+        print('Subscription failed:', reason, file=sys.stderr)
+
+with make_client(endpoint=endpoint, appkey=appkey) as client:
+    print('Connected to Satori RTM!')
+    observer = SubscriptionObserver()
+    client.subscribe('locations', SubscriptionMode.SIMPLE, observer)
+    count = 0
+    try:
+        while count != 7 :
+            time.sleep(1)
+            count = count +1 
+    except KeyboardInterrupt:
+        pass
+
+
+response = client.send_message({'from': '', 'to': '', 'text': 'Please head here to meet emergency services'})
+
+response = response['messages'][0]
+
+if response['status'] == '0':
+  print 'Sent message', response['message-id']
+
+  print 'Remaining balance is', response['remaining-balance']
+else:
+	print 'Error:', response['error-text']
+
+time.sleep(2) #need timer to prevent SMS overflow
+
+response = client.send_message({'from': '', 'to': '', 'text': '%s'%locationem})
+
+response = response['messages'][0]
+
+if response['status'] == '0':
+  print 'Sent message', response['message-id']
+
+  print 'Remaining balance is', response['remaining-balance']
+else:
+	print 'Error:', response['error-text']
