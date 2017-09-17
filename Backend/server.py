9590cd9a623ac9dfa2a81019b850935da074a172
Kent Makishima <khmakishima@ucdavis.edu>
2017-09-16 23:25:03 -0700

    Update server.py

--- Backend/server.py
+++ Backend/server.py
@@ -2,11 +2,11 @@
 
 import nexmo
 
-client = nexmo.Client(key='d4f97f0e', secret='2e02d0a63e14517f')
+client = nexmo.Client(key='', secret='')
 
 # client = nexmo.Client(application_id='a7759aa3-88ef-4bcf-abc3-3c0334cec861', private_key= './private.key')
 
-response = client.send_message({'from': '12012413501', 'to': '19169908919', 'text': 'Are you in a Safe location? What is your location?'})
+response = client.send_message({'from': '', 'to': '', 'text': 'Are you in a Safe location? What is your location?'})
 
 response = response['messages'][0]
 
